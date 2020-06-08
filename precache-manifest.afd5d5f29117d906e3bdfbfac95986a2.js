b3c23407389769b576504d039f02f7d9e669ea98
Pravin P Patil <prvnpatil11@gmail.com>
2020-06-08 17:30:16 +0530

    Updates

---
+++ precache-manifest.afd5d5f29117d906e3bdfbfac95986a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5aa498a8e9e25e171597d357b65e3c9",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "5f212a86f9ea76fabe19",
+    "url": "/react-quiz-app/static/css/main.bfb93718.chunk.css"
+  },
+  {
+    "revision": "b65f23afe90edd943412",
+    "url": "/react-quiz-app/static/js/2.6b7f71d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.6b7f71d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f212a86f9ea76fabe19",
+    "url": "/react-quiz-app/static/js/main.118d2b0d.chunk.js"
+  },
+  {
+    "revision": "5f9ebe21e9892c6e5b70",
+    "url": "/react-quiz-app/static/js/runtime-main.1ae03102.js"
+  }
+]);
